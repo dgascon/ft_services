6a414a6f2ce6f2f7a8bf659acba86bb9a9a0e7bb
dgascon <dgascon@student.42lyon.fr>
2020-09-21 10:29:03 +0200

    Reformat du setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
-#minikube start
+# Mise en place des variables d environnement
 eval $(minikube docker-env)
+
+# Suppression des donnees preexistante
 sh delete.sh
+
+# Installation metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+
+# Application du yaml de metallb
 kubectl apply -f srcs/metallb/metallb.yaml
+
+# Application et build du yaml de nginx
 docker build -t services-nginx srcs/nginx/.
 kubectl apply -f srcs/nginx/nginx.yaml
