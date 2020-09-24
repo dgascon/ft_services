f6a1e008efd48bdc3f123b59e78052589faeadc6
dgascon <dgascon@student.42lyon.fr>
2020-09-24 18:20:25 +0200

    mysql done

--- srcs/mysql/setup.sh
+++
@@ -1,9 +0,0 @@
-apk add openrc
-openrc reboot
-rc-service mariadb start
-echo "CREATE DATABASE wpdb;" | mysql
-echo "CREATE USER 'user42'@'localhost' identified by 'user42';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user42'@'localhost';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-
-/usr/bin/mysqld_safe --datadir='/var/lib/mysql'
